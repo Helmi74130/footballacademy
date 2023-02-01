34a2c16bfa78ca55a5f6f148e784be56e2e5e373
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2023-02-01 10:41:28 +0100

    reservation script

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -27,28 +27,30 @@ class ReservationController extends AbstractController
     public function addReserve(Request $request, EntityManagerInterface $entityManager, TimeRepository $timeRepository): Response
     {
 
+
         $timeReservations = $timeRepository->findAll();
 
         $reservations = new Time();
         $form = $this->createForm(TimerType::class, $reservations);
         $form->handleRequest($request);
         // 5400s = 1h30 en Timestamp
-        $gameTime = 5400;
+        $gameTime = 5300;
 
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             $form->getData();
-            // On Récupère la date et l'heure et on le formate en timeStamp
+            // On Récupère la date et l'heure et on le formate en timeStamp 3600 correspond au chgmt horaire
             $hourTimestampPlayerChoice = $reservations->getHour()->getTimestamp() + 3600;
             $dateTimestampPlayerChoice = $reservations->getDate()->getTimestamp();
-            // On additionne les 2 pour récupérer le temps de début et on ajoute le temps de jeux pour récupérer le temps de fin
+            // On additionne les 2 pour récupérer le temps de début et on déduis le temps de l'équipe précédente et on ajoute le temps de jeux pour récupérer le temps de fin
+
             $startPlay = $hourTimestampPlayerChoice + $dateTimestampPlayerChoice;
             $endPlay = $startPlay + $gameTime;
             $endPlayDateTime = date('Y-m-d H:i:s', $endPlay);
             $startPlayDateTime = date('Y-m-d H:i:s', $startPlay);
 
-
+            //dd($endPlayDateTime);
             $existingReservations = $entityManager->createQuery(
                 "SELECT r FROM App:Time r
                 WHERE r.startplay BETWEEN :start AND :end
@@ -56,6 +58,8 @@ class ReservationController extends AbstractController
             )->setParameter('start', $startPlayDateTime)
                 ->setParameter('end', $endPlayDateTime)
                 ->getResult();
+
+
 
             if (!empty($existingReservations)) {
                 $this->addFlash('danger', 'Créneau horaire non disponible.');
